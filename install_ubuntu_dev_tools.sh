3e45d2918a2b0fde269a212fc3ac7f600eb9ea29
Aubhro Sengupta <hello@aubhro.com>
2025-11-16 18:00:29 -0500

    Add gcc 13 and qemu to ubuntu tools install script

--- install_ubuntu_dev_tools.sh
+++ install_ubuntu_dev_tools.sh
@@ -1,1 +1,12 @@
+sudo apt update
+
+# necessary dependencies for building the kernel
 sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
+
+# install gcc-13 (necessary for building kernel 6.6)
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt update
+sudo apt install gcc-13
+
+# install qemu
+sudo apt install qemu-system
