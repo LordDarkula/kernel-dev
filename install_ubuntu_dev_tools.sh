c8b8e5482bafadbc69dccbe7c0bcb7058847a06c
Aubhro Sengupta <hello@aubhro.com>
2025-11-25 18:00:51 -0800

    Add debhelper to ubuntu install script

--- install_ubuntu_dev_tools.sh
+++ install_ubuntu_dev_tools.sh
@@ -1,12 +1,15 @@
-sudo apt update
+sudo apt update -y
 
 # necessary dependencies for building the kernel
-sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
+sudo apt install -y libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
 
 # install gcc-13 (necessary for building kernel 6.6)
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt update
-sudo apt install gcc-13
+sudo apt update -y
+sudo apt install -y gcc-13
+
+# required for building kernel as debian package
+sudo apt install -y debhelper
 
 # install qemu
-sudo apt install qemu-system
+sudo apt install -y qemu-system
