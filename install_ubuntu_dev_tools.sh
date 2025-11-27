99c46a3379c6c9f95df1aaa5c0773d2e15a0e6f6
Aubhro Sengupta <hello@aubhro.com>
2025-11-26 19:20:26 -0800

    Add per-package install attempts in ubuntu install tools script

--- install_ubuntu_dev_tools.sh
+++ install_ubuntu_dev_tools.sh
@@ -1,15 +1,30 @@
-sudo apt update -y
+packages=(
+    libncurses-dev
+    gawk
+    flex
+    bison
+    openssl
+    libssl-dev
+    dkms
+    libelf-dev
+    libudev-dev
+    libpci-dev
+    autoconf
+    llvm
+		debhelper
+		qemu-system
 
-# necessary dependencies for building the kernel
-sudo apt install -y libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
+)
+sudo apt update
+
+for pkg in "${packages[@]}"; do
+    echo "Installing $pkg..."
+    sudo apt-get install -y --no-install-recommends "$pkg" || \
+        echo "Skipping: $pkg not found or already satisfied"
+done
 
 # install gcc-13 (necessary for building kernel 6.6)
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update -y
 sudo apt install -y gcc-13
 
-# required for building kernel as debian package
-sudo apt install -y debhelper
-
-# install qemu
-sudo apt install -y qemu-system
