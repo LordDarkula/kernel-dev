856bcf818fe887a88e34fa1c5b11806ae1d807bb
Aubhro Sengupta <hello@aubhro.com>
2025-12-17 23:57:28 -0800

    Enable kernel features for colloid in enable colloid script

--- scripts/enable_colloid.sh
+++ scripts/enable_colloid.sh
@@ -9,6 +9,7 @@ echo "Remember to run mimic_cxl_numa.sh to offline NUMA 1."
 
 cd colloid/tpp/
 
+# Build colloid modules
 cd tierinit
 make
 cd ..
@@ -21,6 +22,12 @@ cd colloid-mon
 make
 cd ..
 
+# enable colloid modules
 sudo insmod tierinit/tierinit.ko
 sudo insmod kswapdrst/kswapdrst.ko
 sudo insmod colloid-mon/colloid-mon.ko
+
+# enable colloid features
+sudo swapoff -a # Disable swap
+echo 1 | sudo tee /sys/kernel/mm/numa/demotion_enabled # Enable page demotion
+echo 6 | sudo tee /proc/sys/kernel/numa_balancing # Enable colloid
